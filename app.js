9b23a6ff22c19e7ddb079829805671f282a88f60
cankartal <cankartal.1@gmail.com>
2015-07-28 23:28:14 +0300

    yeni proje basladi

---
+++ app.js
@@ -0,0 +1,30 @@
+var app = angular.module('weather', []);
+
+    app.factory('openweather', function($http) {
+        var runRequest = function(city) {
+            return $http({
+                method: 'JSONP',
+                url: 'http://api.openweathermap.org/data/2.5/forecast/daily?q='+ city + '&mode=json&units=metric&cnt=4&callback=JSON_CALLBACK'
+            });
+        };
+        return {
+            event: function(city) {
+                return runRequest(city);
+            }
+        };
+    });
+
+    app.controller('WeatherForecastCtrl', function($scope, $timeout, openweather){
+        var timeout;
+        $scope.$watch('city', function(newCity) {
+            if(newCity) {
+                if(timeout) $timeout.cancel(timeout);
+                timeout = $timeout(function() {
+                    openweather.event(newCity).success(function(data, status)	{
+                        $scope.loc = data;
+                        $scope.forecast = data.list;
+                    });
+                }, 1000);
+            }
+        });
+    });
